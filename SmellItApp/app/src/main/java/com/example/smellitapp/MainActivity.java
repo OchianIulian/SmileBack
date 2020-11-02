2e2b0e2366c3d2241c2f635ac18fbfb5229f1c77
Okiul <ochian_a@yahoo.ro>
2020-11-02 19:51:58 +0200

    Make splash screen motor

--- SmellItApp/app/src/main/java/com/example/smellitapp/MainActivity.java
+++ SmellItApp/app/src/main/java/com/example/smellitapp/MainActivity.java
@@ -5,23 +5,37 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import com.example.smellitapp.Fragments.Home;
+import com.example.smellitapp.Fragments.Splash;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    private Timer mTimer;
+    private TimerTask mTimerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         openSplash();
 
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
+    }
 
-        Home home = new Home();
-        fragmentTransaction.add(R.id.container, home);
-        fragmentTransaction.commit();
-        //Love
+    void openSplash(){
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new Splash()).commit();
+        mTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new Home()).commit();
+            }
+        };
+
+        mTimer = new Timer();
+        mTimer.schedule(mTimerTask, 10000);
     }
 }
