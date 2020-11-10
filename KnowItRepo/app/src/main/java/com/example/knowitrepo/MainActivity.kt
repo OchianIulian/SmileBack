18bca102e187c7e1f6144cd192dc12e981c700c1
Okiul <ochian_a@yahoo.ro>
2020-11-10 18:25:22 +0200

    Initial Commit

--- KnowItRepo/app/src/main/java/com/example/knowitrepo/MainActivity.kt
+++ KnowItRepo/app/src/main/java/com/example/knowitrepo/MainActivity.kt
@@ -2,20 +2,10 @@ package com.example.knowitrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        getData();
-    }
-
-    fun getData(){
-        val container:ConstraintLayout = findViewById(R.id.container)
     }
 }
