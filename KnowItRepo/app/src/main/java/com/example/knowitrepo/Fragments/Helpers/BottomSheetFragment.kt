ef8d71ea30c5989e8c46c01cd48c43d474047a04
Okiul <ochian_a@yahoo.ro>
2020-11-22 22:30:53 +0200

    Initial commit

--- KnowItRepo/app/src/main/java/com/example/knowitrepo/Fragments/Helpers/BottomSheetFragment.kt
+++ KnowItRepo/app/src/main/java/com/example/knowitrepo/Fragments/Helpers/BottomSheetFragment.kt
@@ -10,20 +10,25 @@ import com.example.knowitrepo.Fragments.Register.ForgotPassword
 import com.example.knowitrepo.Fragments.Register.Register
 import com.example.knowitrepo.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.lang.NullPointerException
 
 class BottomSheetFragment : BottomSheetDialogFragment(){
 
     private lateinit var forgotPassword: TextView
     private lateinit var signUp: TextView
     private lateinit var mainView: View
+    private lateinit var mAuth: FirebaseAuth
+    private var userActive = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var ok =1
-        if(ok==1){
+        mAuth = FirebaseAuth.getInstance()
+        if(!userActive){
             //TODO: log in
             mainView = inflater.inflate(R.layout.layout_persistent_bottom_sheet, container, false)
             getSheetData1(mainView)
@@ -34,6 +39,20 @@ class BottomSheetFragment : BottomSheetDialogFragment(){
         }
 
         return mainView
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var currentUser:FirebaseUser
+        try {
+            currentUser = mAuth.currentUser!!
+            userActive = true
+        } catch ( e: NullPointerException){
+            Toast.makeText(context, "User not connected", Toast.LENGTH_SHORT).show()
+            userActive = false
+        } finally {
+
+        }
     }
 
     private fun onClickListeners1() {
